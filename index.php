f5557fbe2f4abd6b19eb55f9cb9bd70837b4d1a6
seanoxryd <seanoxryd@gmail.com>
2024-02-29 17:39:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>DEV | Floyd Janeo</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	<script src="finisher-header.es5.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<nav>
+		<div class="nav-dev1">
+			<div class="nav-dev2">
+				<div>
+					<a href="index.php#tech-stack">Tech Stack</a>
+					<a href="index.php#projects">Projects</a>
+					<a href="index.php#about-me">About Me</a>
+					<a href="index.php">Contact Me</a>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
+	<div class="header finisher-header" id="header">
+			<script type="text/javascript">
+				new FinisherHeader({
+			  "count": 12,
+			  "size": {
+			    "min": 1300,
+			    "max": 1500,
+			    "pulse": 1
+			  },
+			  "speed": {
+			    "x": {
+			      "min": 1.2,
+			      "max": 5
+			    },
+			    "y": {
+			      "min": 1.2,
+			      "max": 5
+			    }
+			  },
+			  "colors": {
+			    "background": "#953eff",
+			    "particles": [
+			      "#ff681c",
+			      "#87ddfe",
+			      "#231efe",
+			      "#ff0a53"
+			    ]
+			  },
+			  "blending": "lighten",
+			  "opacity": {
+			    "center": 0.6,
+			    "edge": 0
+			  },
+			  "skew": 0,
+			  "shapes": [
+			    "c"
+			  ]
+			});
+		</script>
+		<div class="header-dev1">
+				<h1>Hi, I'm</h1>
+				<h2>Pink Floyd</h2>
+				<h3>Janeo</h3>
+				<p>"It started with me as a student transitioning to a teacher. My journey in Web Design, Photoshop, Photography, and Graphic Art evolved from a personal passion to a professional pursuit. Guiding fellow students sparked my teaching path, which evolved into workshops and classroom roles. Now, I inspire others, sharing expertise and fostering creativity in digital arts."</p>
+				<a href="index.php" class="contact-me">Contact Me</a>
+		</div>
+		<div class="header-dev2">
+			<div class="header-dev2-image">
+				<img src="profile1.png">
+				<div class="icons">
+					<a href="https://www.facebook.com/floydjaneo" alt="Facebook"><i class="fa-brands fa-facebook " id="facebook" alt="Facebook "></i></a>
+					<a href="https://www.instagram.com/something_bad_x"><i class="fa-brands fa-square-instagram" id="instagram"></i></a>
+					<a href="https://www.behance.net/floydjaneo"><i class="fa-brands fa-square-behance" id="behance"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="tech-stack" id="tech-stack" name="tech-stack">
+		<p>TECH STACK</p>
+		<div class="tech-stack-logo">
+			<i class="fa-brands fa-html5"></i>
+			<i class="fa-brands fa-css3"></i>
+			<i class="fa-solid fa-c"></i>
+			<i class="fa-brands fa-java"></i>
+		</div>
+	</div>
+	<div class="projects" id="projects" name="projects">
+		 <p>PROJECTS</p>
+		 <div class="projects-list">
+		 	<!-- Project 1 -->
+			 <div class="project-container">
+			 	<div class="project-container-details">
+			 		<div class="project-container-updown">
+			 			<div class="project-container-data">
+			 				<h1 class="project-name">App Name (Sample)</h1>
+			 				<h2 class="project-name-description">App Name is a versatile productive tool for brainstorming, organizing ideas, and planning projects. With intuitive design and seamless synchronizational, unleash your creativity effortlessly. </h2>
+			 			</div>
+			 			<div class="project-container-data-use">
+			 				<div class="project-app-used">
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 				</div>
+			 			</div>
+			 		</div>
+			 	</div>
+			 	<div class="project-container-image">
+			 		<div class="project-container-image">
+			 			<img src="web1.png" class="project-picture">
+			 		</div>
+			 	</div>
+			 </div>
+
+			 <!-- Project 2 -->
+			 <div class="project-container">
+			 	<div class="project-container-details">
+			 		<div class="project-container-updown">
+			 			<div class="project-container-data">
+			 				<h1 class="project-name">App Name (Sample)</h1>
+			 				<h2 class="project-name-description">App Name is a versatile productive tool for brainstorming, organizing ideas, and planning projects. With intuitive design and seamless synchronizational, unleash your creativity effortlessly. </h2>
+			 			</div>
+			 			<div class="project-container-data-use">
+			 				<div class="project-app-used">
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 				</div>
+			 			</div>
+			 		</div>
+			 	</div>
+			 	<div class="project-container-image">
+			 		<div class="project-container-image">
+			 			<img src="web1.png" class="project-picture">
+			 		</div>
+			 	</div>
+			 </div>
+			 
+			 <!-- Project 3 -->
+			 <div class="project-container">
+			 	<div class="project-container-details">
+			 		<div class="project-container-updown">
+			 			<div class="project-container-data">
+			 				<h1 class="project-name">App Name (Sample)</h1>
+			 				<h2 class="project-name-description">App Name is a versatile productive tool for brainstorming, organizing ideas, and planning projects. With intuitive design and seamless synchronizational, unleash your creativity effortlessly. </h2>
+			 			</div>
+			 			<div class="project-container-data-use">
+			 				<div class="project-app-used">
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 					<h5 class="app-used">Adobe Photoshop</h5>
+			 				</div>
+			 			</div>
+			 		</div>
+			 	</div>
+			 	<div class="project-container-image">
+			 		<div class="project-container-image">
+			 			<img src="web1.png" class="project-picture">
+			 		</div>
+			 	</div>
+			 </div>
+		 </div>
+	</div>
+	<div class="divider">
+		<a href="#_blank"><p class="more">MORE</p></a>
+	</div>
+	<div class="about-me" id="about-me" name="about-me">
+		<div class="me-pic">
+			
+		</div>
+		<div class="me-details">
+			<p>ABOUT ME</p>
+			<h5>"Hi, My name is Pink Floyd Janeo. My grandmother use to tell me stories about the old days, a time of peace when the Avatar kept balance between the Water Tribes, Earth Kingdom, Fire Nation, and Air Nomads. But that all chanced when the Fire Nation attacked. Only the Avatar mastered all four elements. Only he could stop the ruthless firebenders. But when the world needed  him most, he vanished. A hundred years have passed and the Fire Nation is nearing victory in the War. Two years ago, my father and the men of my tribe journeyed to the Earth Kingdom to help fight against the Fire Nation, leaving me and my brother to look after our tribe. Some people believed that the Avatar was never reborn into the Air Nomads, and that the cycle is broken. But I haven't lost hope. I still believe that somehow, the Avatar will return to save the world."</h5>
+		</div>
+	</div>
+</body>
+</html>
